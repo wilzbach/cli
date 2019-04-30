8ace442fc1ed081dca827ea9807b0d35c78675b7
Kenneth Reitz <me@kennethreitz.org>
2019-04-30 08:44:22 -0400

    improvements

--- story/aliases/bootstrap.py
+++ story/aliases/bootstrap.py
@@ -3,12 +3,12 @@ import click
 
 from .. import cli
 from .. import options
-from ..commands import bootstrap
+from ..commands import write
 
 
 @cli.cli.command(name='bootstrap', hidden=True)
 # @options.app()
-@click.argument('story', default='-', type=click.Choice(bootstrap.CHOICES))
+@click.argument('story', default='-', type=click.Choice(write.CHOICES))
 #
 @click.argument(
     'output_file', default=None, type=click.Path(exists=False), required=False
@@ -16,4 +16,4 @@ from ..commands import bootstrap
 @click.pass_context
 def config_list(ctx, **kwargs):
     cli.print_deprecated_warning(alternative='story write')
-    ctx.forward(bootstrap.write)
+    ctx.forward(write.write)
