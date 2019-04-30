98ef042bb57fc18746843d0ce5c528f8a5f7c01b
Kenneth Reitz <me@kennethreitz.org>
2019-04-30 08:58:33 -0400

    black

--- story/commands/version.py
+++ story/commands/version.py
@@ -12,8 +12,11 @@ def version():
     from storyscript import version
 
     click.echo(
-        click.style('Asyncy', fg='magenta') + ' ' +
-        cli.version + click.style(' - ', dim=True) +
-        click.style('Storyscript', fg='cyan') + ' ' +
-        version
+        click.style('Asyncy', fg='magenta')
+        + ' '
+        + cli.version
+        + click.style(' - ', dim=True)
+        + click.style('Storyscript', fg='cyan')
+        + ' '
+        + version
     )
