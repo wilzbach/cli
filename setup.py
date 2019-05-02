e007586638268bf4b72c44162d270df8a77f5f8c
Sebastian Wilzbach <seb@wilzba.ch>
2019-05-02 09:10:12 -0400

    Update setup.py
    Co-Authored-By: kenneth-reitz <me@kennethreitz.org>

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     classifiers=classifiers,
-    download_url='https://github.com/storyscript/cli/archive/master.zip',
+    download_url=f'https://github.com/storyscript/cli/archive/{version}.zip',
     keywords=' '.join(keywords),
     author='Storyscript',
     author_email='hello@storyscript.io',
