c1315760d6d886bd9abc3d8d20f59e6c6e77b354
Sebastian Wilzbach <seb@wilzba.ch>
2019-05-23 11:45:23 +0200

    Add docker build

---
+++ snap/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd "$DIR"
+
+docker build -t story-snapcraft .
+
+docker run --rm -v $PWD/../:/story story-snapcraft /bin/sh -c 'cd /story && snapcraft clean && snapcraft'
