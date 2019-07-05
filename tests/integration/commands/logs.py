8c294ce2469d34889490fd0d445fdf058fa05354
Jude Pereira <contact@judepereira.com>
2019-07-05 16:50:42 +0200

    Add tests for connect_and_listen_with_retry, ping_forever.

--- tests/integration/commands/logs.py
+++ tests/integration/commands/logs.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 import asyncio
+from unittest import mock
+from urllib.error import URLError
+
+import click
 
 from pytest import mark
 
 from story import api, cli
+
+from websockets import ConnectionClosed, InvalidStatusCode
 
 
 @mark.parametrize('level', [None, 'info', 'debug', 'warning', 'error'])
@@ -81,3 +87,74 @@ def test_logs_command(runner, init_sample_app_in_cwd, patch, level, follow,
     asyncio.get_event_loop.return_value.run_until_complete.assert_called_with(
         logs.connect_and_listen_with_retry.return_value
     )
+
+
+@mark.parametrize('exception_to_throw', [URLError('reason'),
+                                         ConnectionClosed(10, 'closed')])
+@mark.asyncio
+async def test_ping_forever(runner, init_sample_app_in_cwd, magic, patch,
+                            async_mock, exception_to_throw):
+    websocket = magic()
+    patch.object(asyncio, 'sleep', new=async_mock())
+    patch.object(websocket, 'send', new=async_mock(side_effect=[
+        None, None, None, None, exception_to_throw]))
+    with runner.runner.isolated_filesystem():
+        init_sample_app_in_cwd()
+        from story.commands.logs import ping_forever
+        await ping_forever(websocket)
+
+    assert websocket.send.mock.mock_calls == [
+        mock.call('{"command": "ping"}'),
+        mock.call('{"command": "ping"}'),
+        mock.call('{"command": "ping"}'),
+        mock.call('{"command": "ping"}'),
+        mock.call('{"command": "ping"}')
+    ]
+
+    assert asyncio.sleep.mock.call_count == 4
+
+
+@mark.parametrize('final_result_of_connect_once', [
+    URLError('reason'),
+    InvalidStatusCode(10),
+    InvalidStatusCode(500),
+    True
+])
+@mark.asyncio
+async def test_connect_and_listen_with_retry(runner, init_sample_app_in_cwd,
+                                             patch, async_mock,
+                                             final_result_of_connect_once):
+    patch.object(click, 'get_current_context')
+
+    with runner.runner.isolated_filesystem():
+        init_sample_app_in_cwd()
+
+        from story.commands import logs
+
+        patch.object(logs, 'connect_and_listen_once',
+                     new=async_mock(side_effect=[
+                         False,
+                         False,
+                         False,
+                         False,
+                         final_result_of_connect_once]))
+
+        await logs.connect_and_listen_with_retry(
+            'app_id', 'n', 'follow', 'runtime_logs', 'service_logs',
+            'service_name', 'level')
+
+    assert logs.connect_and_listen_once.mock.mock_calls == [
+        mock.call('app_id', 'n', 'follow', 'runtime_logs', 'service_logs',
+                  'service_name', 'level'),
+        mock.call('app_id', 100, 'follow', 'runtime_logs', 'service_logs',
+                  'service_name', 'level'),
+        mock.call('app_id', 100, 'follow', 'runtime_logs', 'service_logs',
+                  'service_name', 'level'),
+        mock.call('app_id', 100, 'follow', 'runtime_logs', 'service_logs',
+                  'service_name', 'level'),
+        mock.call('app_id', 100, 'follow', 'runtime_logs', 'service_logs',
+                  'service_name', 'level')
+    ]
+
+    if isinstance(final_result_of_connect_once, BaseException):
+        click.get_current_context.return_value.exit.assert_called_with(1)
