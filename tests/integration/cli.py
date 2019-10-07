f52d811542098818a075a6eca0cd5de33b2c6b23
Artur Tamborski <tamborskiartur@gmail.com>
2019-10-07 11:53:46 +0200

    Fix linting errors

--- tests/integration/cli.py
+++ tests/integration/cli.py
@@ -6,14 +6,14 @@ from story.cli import cli
 
 def test_cli_as_module(spawn_process):
     # We don't need space-indents from docstrings
-    output = str(cli.__doc__).replace("  ", " ").strip()
+    output = str(cli.__doc__).replace('  ', ' ').strip()
 
     # Python executable path on Windows should be quoted
-    path = '"{}"' if sys.platform == "win32" else "{}"
+    path = '"{}"' if sys.platform == 'win32' else '{}'
     python = path.format(sys.executable)
 
-    exit_code, stdout, stderr = spawn_process([python, "-m", "story"])
+    exit_code, stdout, stderr = spawn_process([python, '-m', 'story'])
 
     assert exit_code == 0
     assert output in stdout
-    assert "" in stderr
+    assert '' in stderr
