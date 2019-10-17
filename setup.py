51251764b9ce9a0cbcb2cfe4d3f2156384d31553
Sebastian Wilzbach <seb@wilzba.ch>
2019-10-17 05:56:47 +0200

    Add upload command

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@
 import io
 import os
 import sys
+from shutil import rmtree
+import subprocess
 
 from setuptools.command.install import install as _install
 from setuptools import find_packages, setup, Command
 
+
 from story.version import version
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Install(_install):
@@ -48,6 +53,42 @@ class Format(Command):
 
     def run(self):
         os.system('black -S -l 79 story')
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(root_dir, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        subprocess.run(
+            [sys.executable, 'setup.py', 'sdist', 'bdist_wheel',
+             '--universal'],
+            check=True)
+
+        self.status('Uploading the package to PyPI via Twine…')
+        subprocess.run(['twine', 'upload', 'dist/*'], check=True, shell=True)
+
+        sys.exit()
 
 
 classifiers = [
@@ -126,5 +167,10 @@ setup(
     extras_require={},
     requires_python='>=3.6.0',
     entry_points={'console_scripts': ['story=story.__main__:cli']},
-    cmdclass={'install': Install, 'test': PyTest, 'format': Format},
+    cmdclass={
+        'format': Format,
+        'install': Install,
+        'upload': UploadCommand,
+        'test': PyTest,
+    },
 )
