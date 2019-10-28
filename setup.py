20aabd035dff60b55e97f42170b020b901679026
Storyscript Infrastructure <infra@storyscript.io>
2019-10-28 15:59:43 +0000

    Upgrade Storyscript to 0.25.6
    Details: https://github.com/storyscript/storyscript/releases/tag/0.25.6

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ requirements = [
     'emoji==0.5.0',
     'raven==6.9.0',
     'requests~=2.20',
-    'storyscript==0.24.2',
+    'storyscript==0.25.6',
     'websockets~=7.0',
     'texttable~=1.4.0',
     'pyyaml>=4.2',
